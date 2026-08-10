280afb7b85285b2ada78079e956d10bc35d34675
Anuj kumar <anujk396590@gmail.com>
2026-08-10 16:25:35 +0530

    Update API baseURL to Render production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://community-share-backend.onrender.com/api'
-  //  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://community-share-backend.onrender.com/api/'
 });
 
 api.interceptors.request.use((config) => {
